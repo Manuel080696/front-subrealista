011ac003c2e74d508720ae1ddaf1fcc20514a195
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-03-24 12:15:30 +0100

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#fbf2d5",
+				secondary: "#068da9",
+				tertiary: "#fdc57b",
+				quaternary: "#394a51",
+			},
+			fontFamily: {
+				body: ["IBM Plex Sans", "sans-serif"],
+				title: ["IBM Plex Sans Condensed", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
