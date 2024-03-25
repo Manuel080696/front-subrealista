e593544c914b58533405b25cc27752b681547650
Manuel Casa <mgmanugonza@gmail.com>
2024-03-25 23:43:44 +0100

    Arreglo en header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -11,7 +11,7 @@ import { useState } from "react";
 export function Header() {
   const [active, setActive] = useState(false);
   return (
-    <header className="min-h-min h-min fixed bottom-0 z-50 bg-[var(--primary-color)] md:top-0 md:sticky md:py-5 md:border-solid md:border-b-2 md:drop-shadow-sm">
+    <header className="min-h-min h-min fixed bottom-0 z-50 bg-[var(--primary-color)] md:top-0 md:sticky md:pt-2 md:pb-5 md:border-solid md:border-b-2 md:drop-shadow-sm">
       {/*Header de móvil*/}
       <ul className="flex flex-row w-screen justify-center items-center h-16 text-[var(--quintanary-color)] text-xs md:hidden">
         <li className="w-24 active:text-[var(--quaternary-color)]   ">
@@ -45,7 +45,7 @@ export function Header() {
 
       {/*Header de PC*/}
       <nav className="hidden md:flex md:flex-col md:w-screen md:px-20 md:items-center md:h-min md:text-[var(--quintanary-color)] md:text-xs md:gap-y-1">
-        <section className="flex flex-row justify-between w-full">
+        <section className="flex flex-row justify-between items-center w-full">
           <Link
             to="/"
             className="flex flex-col justify-center items-center h-3/5"
