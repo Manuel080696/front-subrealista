95b6846a915d6754855a6cc19b0fd6ebf9b62a99
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-04-11 17:04:30 +0200

    Revert "fixed issue with naming and context providing"
    This reverts commit 21768c0904c959d277589f31c622480500157df5.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,149 +8,149 @@ import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import AccountMenu from "./account-menu";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useContext, useState } from "react";
-import { CurrentUserContext } from "../context/auth-context";
+import { AuthContextProvider } from "../Context/auth-context";
 import { SearchMobile } from "./search-mobile";
 
 export function Header({ handleFilteredPosts, isOpen, setIsOpen }) {
-	const [active, setActive] = useState(false);
-	const { user, userData } = useContext(CurrentUserContext);
+  const [active, setActive] = useState(false);
+  const { user, userData } = useContext(CurrentUserContext);
 
-	return userData === null ? (
-		<header className="min-h-min w-full h-min fixed bottom-0 z-50 bg-[var(--primary-color)] md:top-0 md:sticky md:pt-2 md:pb-5 md:border-solid md:border-b-2 md:drop-shadow-sm">
-			{/*Header de móvil*/}
-			<ul className="flex flex-row w-screen justify-center items-center h-16 text-[var(--quintanary-color)] text-xs md:hidden">
-				<li className="w-24 active:text-[var(--quaternary-color)]   ">
-					<Link
-						to="/"
-						onClick={() => handleFilteredPosts("")}
-						className="flex flex-col justify-center items-center h-3/5"
-					>
-						<SearchIcon className="text-gray" />
-						<p className="my-1 font-light">Explorar</p>
-					</Link>
-				</li>
-				<li className="w-24 active:text-[var(--quaternary-color)]">
-					<Link
-						to="/login"
-						className="flex flex-col justify-center items-center h-3/5"
-					>
-						<FavoriteBorderIcon className="text-gray" />
-						<p className="my-1 font-light">Favoritos</p>
-					</Link>
-				</li>
-				<li className="w-24 active:text-[var(--quaternary-color)] ">
-					<Link
-						to="/register"
-						className="w-24 h-full flex flex-col justify-center items-center"
-					>
-						<AccountCircleOutlinedIcon className="text-gray" />
-						<p className="my-1 font-light">Iniciar Sesión</p>
-					</Link>
-				</li>
-			</ul>
-			<section className="absolute">
-				<SearchMobile
-					isOpen={isOpen}
-					setIsOpen={setIsOpen}
-					handleFilteredPosts={handleFilteredPosts}
-				/>
-			</section>
+  return userData === null ? (
+    <header className="min-h-min w-full h-min fixed bottom-0 z-50 bg-[var(--primary-color)] md:top-0 md:sticky md:pt-2 md:pb-5 md:border-solid md:border-b-2 md:drop-shadow-sm">
+      {/*Header de móvil*/}
+      <ul className="flex flex-row w-screen justify-center items-center h-16 text-[var(--quintanary-color)] text-xs md:hidden">
+        <li className="w-24 active:text-[var(--quaternary-color)]   ">
+          <Link
+            to="/"
+            onClick={() => handleFilteredPosts("")}
+            className="flex flex-col justify-center items-center h-3/5"
+          >
+            <SearchIcon className="text-gray" />
+            <p className="my-1 font-light">Explorar</p>
+          </Link>
+        </li>
+        <li className="w-24 active:text-[var(--quaternary-color)]">
+          <Link
+            to="/login"
+            className="flex flex-col justify-center items-center h-3/5"
+          >
+            <FavoriteBorderIcon className="text-gray" />
+            <p className="my-1 font-light">Favoritos</p>
+          </Link>
+        </li>
+        <li className="w-24 active:text-[var(--quaternary-color)] ">
+          <Link
+            to="/register"
+            className="w-24 h-full flex flex-col justify-center items-center"
+          >
+            <AccountCircleOutlinedIcon className="text-gray" />
+            <p className="my-1 font-light">Iniciar Sesión</p>
+          </Link>
+        </li>
+      </ul>
+      <section className="absolute">
+        <SearchMobile
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          handleFilteredPosts={handleFilteredPosts}
+        />
+      </section>
 
-			{/*Header de PC*/}
-			<nav className="hidden md:flex md:flex-col md:w-screen md:px-20 md:items-center md:h-min md:text-[var(--quintanary-color)] md:text-xs md:gap-y-1">
-				<section className="flex flex-row justify-between items-center w-full">
-					<Link
-						to="/"
-						onClick={() => handleFilteredPosts("")}
-						className="flex flex-col justify-center items-center h-3/5"
-					>
-						<img src="/logo/logo.webp" alt="search" className="w-24" />
-					</Link>
-					<button
-						onClick={() => setActive(!active)}
-						className={`flex flex-row h-10 px-3 py-6 gap-2 border border-zinc-400 rounded-full justify-center items-center relative bg-white z-20 ${
-							active ? "drop-shadow-lg" : "hover:drop-shadow-lg"
-						}`}
-					>
-						<MenuIcon sx={{ fontSize: "1.5rem" }} />
-						<AccountCircleIcon sx={{ fontSize: "2.5rem" }} />
-						<AccountMenu active={active} user={user} />
-					</button>
-				</section>
-				<section className="flex flex-col justify-center items-center min-w-min w-6/12 z-10">
-					<Search handleFilteredPosts={handleFilteredPosts} />
-				</section>
-			</nav>
-		</header>
-	) : (
-		<header className="min-h-min w-full h-min fixed bottom-0 z-50 bg-[var(--primary-color)] md:top-0 md:sticky md:pt-2 md:pb-5 md:border-solid md:border-b-2 md:drop-shadow-sm">
-			{/*Header de móvil*/}
-			<ul className="flex flex-row w-screen justify-center items-center h-16 text-[var(--quintanary-color)] text-xs md:hidden">
-				<li className="w-24 active:text-[var(--quaternary-color)]   ">
-					<Link
-						to="/"
-						className="flex flex-col justify-center items-center h-3/5"
-					>
-						<SearchIcon className="text-gray" />
-						<p className="my-1 font-light">Explorar</p>
-					</Link>
-				</li>
-				<li className="w-24 active:text-[var(--quaternary-color)]">
-					<Link
-						to="/favorites"
-						className="flex flex-col justify-center items-center h-3/5"
-					>
-						<FavoriteBorderIcon className="text-gray" />
-						<p className="my-1 font-light">Favoritos</p>
-					</Link>
-				</li>
-				<li className="w-24 active:text-[var(--quaternary-color)] ">
-					<Link
-						to={`/users/${user?.username}`}
-						className="w-24 h-full flex flex-col justify-center items-center"
-					>
-						<Avatar
-							alt="Foto de perfil"
-							src={userData?.profilePic}
-							sx={{ width: 24, height: 24 }}
-						/>
-						<p className="my-1 font-light">Iniciar Sesión</p>
-					</Link>
-				</li>
-			</ul>
-			<SearchMobile
-				isOpen={isOpen}
-				setIsOpen={setIsOpen}
-				handleFilteredPosts={handleFilteredPosts}
-			/>
+      {/*Header de PC*/}
+      <nav className="hidden md:flex md:flex-col md:w-screen md:px-20 md:items-center md:h-min md:text-[var(--quintanary-color)] md:text-xs md:gap-y-1">
+        <section className="flex flex-row justify-between items-center w-full">
+          <Link
+            to="/"
+            onClick={() => handleFilteredPosts("")}
+            className="flex flex-col justify-center items-center h-3/5"
+          >
+            <img src="/logo/logo.webp" alt="search" className="w-24" />
+          </Link>
+          <button
+            onClick={() => setActive(!active)}
+            className={`flex flex-row h-10 px-3 py-6 gap-2 border border-zinc-400 rounded-full justify-center items-center relative bg-white z-20 ${
+              active ? "drop-shadow-lg" : "hover:drop-shadow-lg"
+            }`}
+          >
+            <MenuIcon sx={{ fontSize: "1.5rem" }} />
+            <AccountCircleIcon sx={{ fontSize: "2.5rem" }} />
+            <AccountMenu active={active} user={user} />
+          </button>
+        </section>
+        <section className="flex flex-col justify-center items-center min-w-min w-6/12 z-10">
+          <Search handleFilteredPosts={handleFilteredPosts} />
+        </section>
+      </nav>
+    </header>
+  ) : (
+    <header className="min-h-min w-full h-min fixed bottom-0 z-50 bg-[var(--primary-color)] md:top-0 md:sticky md:pt-2 md:pb-5 md:border-solid md:border-b-2 md:drop-shadow-sm">
+      {/*Header de móvil*/}
+      <ul className="flex flex-row w-screen justify-center items-center h-16 text-[var(--quintanary-color)] text-xs md:hidden">
+        <li className="w-24 active:text-[var(--quaternary-color)]   ">
+          <Link
+            to="/"
+            className="flex flex-col justify-center items-center h-3/5"
+          >
+            <SearchIcon className="text-gray" />
+            <p className="my-1 font-light">Explorar</p>
+          </Link>
+        </li>
+        <li className="w-24 active:text-[var(--quaternary-color)]">
+          <Link
+            to="/favorites"
+            className="flex flex-col justify-center items-center h-3/5"
+          >
+            <FavoriteBorderIcon className="text-gray" />
+            <p className="my-1 font-light">Favoritos</p>
+          </Link>
+        </li>
+        <li className="w-24 active:text-[var(--quaternary-color)] ">
+          <Link
+            to={`/users/${user?.username}`}
+            className="w-24 h-full flex flex-col justify-center items-center"
+          >
+            <Avatar
+              alt="Foto de perfil"
+              src={userData?.profilePic}
+              sx={{ width: 24, height: 24 }}
+            />
+            <p className="my-1 font-light">Iniciar Sesión</p>
+          </Link>
+        </li>
+      </ul>
+      <SearchMobile
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        handleFilteredPosts={handleFilteredPosts}
+      />
 
-			{/*Header de PC*/}
-			<nav className="hidden md:flex md:flex-col md:w-screen md:px-20 md:items-center md:h-min md:text-[var(--quintanary-color)] md:text-xs md:gap-y-1">
-				<section className="flex flex-row justify-between items-center w-full">
-					<Link
-						to="/"
-						onClick={() => handleFilteredPosts("")}
-						className="flex flex-col justify-center items-center h-3/5"
-					>
-						<img src="/logo/logo.webp" alt="search" className="w-24" />
-					</Link>
-					<button
-						onClick={() => setActive(!active)}
-						className={`flex flex-row h-10 px-3 py-6 gap-2 border border-zinc-400 rounded-full justify-center items-center relative bg-white z-20 ${
-							active ? "drop-shadow-lg" : "hover:drop-shadow-lg"
-						}`}
-					>
-						<MenuIcon sx={{ fontSize: "1.5rem" }} />
-						<Avatar alt="Foto de perfil" src={userData?.profilePic} />
-						<AccountMenu active={active} user={user} />
-					</button>
-				</section>
-				<section className="flex flex-col justify-center items-center min-w-[42rem] w-6/12 z-10 pt-5">
-					<Search handleFilteredPosts={handleFilteredPosts} />
-				</section>
-			</nav>
-		</header>
-	);
+      {/*Header de PC*/}
+      <nav className="hidden md:flex md:flex-col md:w-screen md:px-20 md:items-center md:h-min md:text-[var(--quintanary-color)] md:text-xs md:gap-y-1">
+        <section className="flex flex-row justify-between items-center w-full">
+          <Link
+            to="/"
+            onClick={() => handleFilteredPosts("")}
+            className="flex flex-col justify-center items-center h-3/5"
+          >
+            <img src="/logo/logo.webp" alt="search" className="w-24" />
+          </Link>
+          <button
+            onClick={() => setActive(!active)}
+            className={`flex flex-row h-10 px-3 py-6 gap-2 border border-zinc-400 rounded-full justify-center items-center relative bg-white z-20 ${
+              active ? "drop-shadow-lg" : "hover:drop-shadow-lg"
+            }`}
+          >
+            <MenuIcon sx={{ fontSize: "1.5rem" }} />
+            <Avatar alt="Foto de perfil" src={userData?.profilePic} />
+            <AccountMenu active={active} user={user} />
+          </button>
+        </section>
+        <section className="flex flex-col justify-center items-center min-w-[42rem] w-6/12 z-10 pt-5">
+          <Search handleFilteredPosts={handleFilteredPosts} />
+        </section>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
--- src/pages/user-page.jsx
+++ src/pages/user-page.jsx
@@ -1,9 +1,9 @@
 import React, {
-	useEffect,
-	useRef,
-	useState,
-	useCallback,
-	useContext,
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  useContext,
 } from "react";
 import { Rating } from "@mui/material";
 import { Link, useNavigate, useParams } from "react-router-dom";
@@ -17,95 +17,95 @@ import { useCurrentUser } from "../hooks/use-current-user";
 import { toast } from "sonner";
 import { getUserRents } from "../services/get-all-rents-by-username";
 import EditIcon from "@mui/icons-material/Edit";
-import { CurrentUserContext } from "../context/auth-context";
+import { AuthContextProvider } from "../Context/auth-context";
 import Carousel from "../components/carousel";
 import { getRentData } from "../services/get-rent-data";
 import { getAllImages } from "../services/get-all-images";
 export function UserPage() {
-	const navigate = useNavigate();
-	const { username } = useParams();
-	const [error, setError] = useState(null);
-	const [rents, setRents] = useState([]);
-	const [images, setImages] = useState([]);
-	const { userData } = useContext(CurrentUserContext);
-	const [isHovered, setIsHovered] = useState(false);
-	const fileInputRef = useRef(null);
-	const [accountOwnership, setAccountOwnership] = useState(false);
-	const currentUser = useCurrentUser();
-	const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 768);
-	const shortDate = dayjs(userData?.createdAt).format("DD/MM/YYYY");
-	console.log(userData);
+  const navigate = useNavigate();
+  const { username } = useParams();
+  const [error, setError] = useState(null);
+  const [rents, setRents] = useState([]);
+  const [images, setImages] = useState([]);
+  const { userData } = useContext(CurrentUserContext);
+  const [isHovered, setIsHovered] = useState(false);
+  const fileInputRef = useRef(null);
+  const [accountOwnership, setAccountOwnership] = useState(false);
+  const currentUser = useCurrentUser();
+  const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 768);
+  const shortDate = dayjs(userData?.createdAt).format("DD/MM/YYYY");
+  console.log(userData);
 
-	useEffect(() => {
-		const handleResize = () => {
-			setIsMobileView(window.innerWidth <= 768);
-		};
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobileView(window.innerWidth <= 768);
+    };
 
-		window.addEventListener("resize", handleResize);
+    window.addEventListener("resize", handleResize);
 
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-	const fetchUserRents = useCallback(async () => {
-		if (userData) {
-			const result = await getUserRents(userData.username);
-			if (result?.status === "ok") {
-				setRents(result?.data);
+  const fetchUserRents = useCallback(async () => {
+    if (userData) {
+      const result = await getUserRents(userData.username);
+      if (result?.status === "ok") {
+        setRents(result?.data);
 
-				// Obtener todas las imágenes para cada publicación de forma concurrente
-				const promises = result.data.map(async (rent) => {
-					const imagesResult = await getAllImages(rent?.rent_id);
-					if (imagesResult?.status === "ok") {
-						return {
-							rentId: rent?.rent_id,
-							images: imagesResult?.data[1],
-						};
-					}
-					return null;
-				});
+        // Obtener todas las imágenes para cada publicación de forma concurrente
+        const promises = result.data.map(async (rent) => {
+          const imagesResult = await getAllImages(rent?.rent_id);
+          if (imagesResult?.status === "ok") {
+            return {
+              rentId: rent?.rent_id,
+              images: imagesResult?.data[1],
+            };
+          }
+          return null;
+        });
 
-				// Esperar a que todas las promesas se resuelvan
-				const imagesData = await Promise.all(promises);
+        // Esperar a que todas las promesas se resuelvan
+        const imagesData = await Promise.all(promises);
 
-				// Filtrar y eliminar elementos nulos o duplicados
-				const uniqueImagesData = imagesData.filter(
-					(item, index, self) =>
-						item && index === self.findIndex((t) => t?.rentId === item?.rentId)
-				);
+        // Filtrar y eliminar elementos nulos o duplicados
+        const uniqueImagesData = imagesData.filter(
+          (item, index, self) =>
+            item && index === self.findIndex((t) => t?.rentId === item?.rentId)
+        );
 
-				// Establecer el estado de imágenes con los datos únicos
-				setImages(uniqueImagesData);
-			}
-		}
-	}, [username]);
+        // Establecer el estado de imágenes con los datos únicos
+        setImages(uniqueImagesData);
+      }
+    }
+  }, [username]);
 
-	const checkOwnership = useCallback(() => {
-		if (username === currentUser?.user?.username) {
-			setAccountOwnership(true);
-		}
-	}, [username, currentUser]);
+  const checkOwnership = useCallback(() => {
+    if (username === currentUser?.user?.username) {
+      setAccountOwnership(true);
+    }
+  }, [username, currentUser]);
 
-	useEffect(() => {
-		fetchUserRents();
-		checkOwnership();
-	}, [checkOwnership, currentUser]);
-	const handleMouseEnter = () => {
-		setIsHovered(true);
-	};
+  useEffect(() => {
+    fetchUserRents();
+    checkOwnership();
+  }, [checkOwnership, currentUser]);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
 
-	const handleMouseLeave = () => {
-		setIsHovered(false);
-	};
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
-	const handleCameraIconClick = () => {
-		if (fileInputRef) {
-			fileInputRef.current.click();
-		}
-	};
+  const handleCameraIconClick = () => {
+    if (fileInputRef) {
+      fileInputRef.current.click();
+    }
+  };
 
-	/*   const handleFileInputChange = async (e) => {
+  /*   const handleFileInputChange = async (e) => {
     const file = e.target.files[0];
     const userId = userData.id;
     if (file && userId) {
@@ -126,149 +126,149 @@ export function UserPage() {
     }
   }; */
 
-	return (
-		<Main>
-			<article className="flex w-full max-w-screen-2xl">
-				<section className="flex flex-col w-full pb-10 md:flex-row border-b">
-					<Link
-						to={`/users/${userData?.username}/update`}
-						className="flex flex-col justify-center items-end mb-5 font-semibold underline md:hidden"
-					>
-						Editar perfil
-					</Link>
-					<span
-						className="flex flex-col md:w-2/5 md:min-w-72 bg-white rounded-lg shadow-md p-5"
-						onMouseEnter={handleMouseEnter}
-						onMouseLeave={handleMouseLeave}
-						style={{
-							position: "relative",
-							alignItems: "center",
-							justifyContent: "center",
-						}}
-					>
-						{!isHovered ||
-							(accountOwnership && (
-								<IconButton
-									style={{
-										position: "absolute",
-										top: "50%",
-										left: "50%",
-										transform: "translate(-50%, -50%)",
-										background: "rgba(255, 255, 255, 0.7)",
-										display: "flex",
-										justifyContent: "center",
-										alignItems: "center",
-									}}
-									onClick={handleCameraIconClick}
-								>
-									<PhotoCameraIcon />
-									<input
-										type="file"
-										accept="image/*"
-										style={{ display: "none" }}
-										ref={fileInputRef}
-									/>
-								</IconButton>
-							))}
+  return (
+    <Main>
+      <article className="flex w-full max-w-screen-2xl">
+        <section className="flex flex-col w-full pb-10 md:flex-row border-b">
+          <Link
+            to={`/users/${userData?.username}/update`}
+            className="flex flex-col justify-center items-end mb-5 font-semibold underline md:hidden"
+          >
+            Editar perfil
+          </Link>
+          <span
+            className="flex flex-col md:w-2/5 md:min-w-72 bg-white rounded-lg shadow-md p-5"
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            style={{
+              position: "relative",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            {!isHovered ||
+              (accountOwnership && (
+                <IconButton
+                  style={{
+                    position: "absolute",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    background: "rgba(255, 255, 255, 0.7)",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                  onClick={handleCameraIconClick}
+                >
+                  <PhotoCameraIcon />
+                  <input
+                    type="file"
+                    accept="image/*"
+                    style={{ display: "none" }}
+                    ref={fileInputRef}
+                  />
+                </IconButton>
+              ))}
 
-						<img
-							className="w-32 h-32 rounded-full mx-auto object-cover"
-							src={userData?.profilePic}
-							alt={"Foto de " + userData?.username}
-						/>
+            <img
+              className="w-32 h-32 rounded-full mx-auto object-cover"
+              src={userData?.profilePic}
+              alt={"Foto de " + userData?.username}
+            />
 
-						<h2 className="text-center text-2xl font-semibold mt-3">
-							{userData?.username}
-						</h2>
-						<h3 className="text-center text-gray-600 mt-1">
-							{userData?.media_valoracion ? (
-								<span className="flex align-middle justify-center">
-									<Rating value={userData?.media_valoracion} readOnly /> (
-									{userData?.media_valoracion})
-								</span>
-							) : (
-								"Sin valoraciones"
-							)}
-						</h3>
+            <h2 className="text-center text-2xl font-semibold mt-3">
+              {userData?.username}
+            </h2>
+            <h3 className="text-center text-gray-600 mt-1">
+              {userData?.media_valoracion ? (
+                <span className="flex align-middle justify-center">
+                  <Rating value={userData?.media_valoracion} readOnly /> (
+                  {userData?.media_valoracion})
+                </span>
+              ) : (
+                "Sin valoraciones"
+              )}
+            </h3>
 
-						<span className="flex justify-center mt-5">
-							<p>Usuario desde: {shortDate}</p>
-						</span>
-					</span>
+            <span className="flex justify-center mt-5">
+              <p>Usuario desde: {shortDate}</p>
+            </span>
+          </span>
 
-					<section className="flex flex-col px-10 w-full pt-4">
-						<h2 className="text-3xl font-bold text-center mt-5 md:text-start">{`Información de ${userData?.username}`}</h2>
-						{accountOwnership ? (
-							<button
-								onClick={() => navigate(`/users/${userData?.username}/update`)}
-								className="hidden md:flex md:flex-col md:items-center md:text-xs md:mt-5 md:font-semibold md:justify-center md:w-full md:border md:border-black md:p-2 md:rounded-lg md:min-w-28 md:max-w-28 md:w-2/5"
-							>
-								{isMobileView ? <EditIcon /> : "Editar tu perfil"}
-							</button>
-						) : null}
-						<ul className="flex flex-col mt-5 gap-5">
-							{userData ? (
-								userData.bio?.length || userData.address?.length !== 0 ? (
-									<React.Fragment>
-										{userData.address && userData.address.length !== 0 && (
-											<li className="flex flex-row gap-2">
-												<LocationOnIcon />
-												<p>{userData.address}</p>
-											</li>
-										)}
-										{userData.bio && userData.bio.length !== 0 && (
-											<p>{userData.bio}</p>
-										)}
-									</React.Fragment>
-								) : (
-									<p>
-										Aún no se han añadido datos en tu perfil,{" "}
-										<Link
-											to={`/users/${userData.username}/update`}
-											className="underline font-semibold"
-										>
-											haz click aquí
-										</Link>
-										.
-									</p>
-								)
-							) : null}
-							{/* {language.length !== 0 ? (
+          <section className="flex flex-col px-10 w-full pt-4">
+            <h2 className="text-3xl font-bold text-center mt-5 md:text-start">{`Información de ${userData?.username}`}</h2>
+            {accountOwnership ? (
+              <button
+                onClick={() => navigate(`/users/${userData?.username}/update`)}
+                className="hidden md:flex md:flex-col md:items-center md:text-xs md:mt-5 md:font-semibold md:justify-center md:w-full md:border md:border-black md:p-2 md:rounded-lg md:min-w-28 md:max-w-28 md:w-2/5"
+              >
+                {isMobileView ? <EditIcon /> : "Editar tu perfil"}
+              </button>
+            ) : null}
+            <ul className="flex flex-col mt-5 gap-5">
+              {userData ? (
+                userData.bio?.length || userData.address?.length !== 0 ? (
+                  <React.Fragment>
+                    {userData.address && userData.address.length !== 0 && (
+                      <li className="flex flex-row gap-2">
+                        <LocationOnIcon />
+                        <p>{userData.address}</p>
+                      </li>
+                    )}
+                    {userData.bio && userData.bio.length !== 0 && (
+                      <p>{userData.bio}</p>
+                    )}
+                  </React.Fragment>
+                ) : (
+                  <p>
+                    Aún no se han añadido datos en tu perfil,{" "}
+                    <Link
+                      to={`/users/${userData.username}/update`}
+                      className="underline font-semibold"
+                    >
+                      haz click aquí
+                    </Link>
+                    .
+                  </p>
+                )
+              ) : null}
+              {/* {language.length !== 0 ? (
                 <li className="flex flex-row">
                   <LocationOnIcon />
                   <p>{userData?.address}</p>
                 </li>
               ) : null} */}
-						</ul>
-					</section>
-				</section>
-			</article>
+            </ul>
+          </section>
+        </section>
+      </article>
 
-			{accountOwnership ? (
-				<section className="flex gap-4 justify-around"></section>
-			) : (
-				""
-			)}
+      {accountOwnership ? (
+        <section className="flex gap-4 justify-around"></section>
+      ) : (
+        ""
+      )}
 
-			<section className="mb-3 mt-3">
-				<h2 className="text-3xl font-bold text-center pt-3 pb-5 md:pt-0">
-					Mis Posts
-				</h2>
-				<ul className="flex flex-row flex-wrap basis-20 w-full justify-around gap-3 max-w-screen-2xl md:flex-nowrap">
-					{rents?.map((image) => {
-						const rentImages = images.find(
-							(item) => item.rentId === image.rent_id
-						);
-						return (
-							<li key={image.rent_id} className="w-full">
-								<Carousel
-									images={rentImages?.length !== 0 ? rentImages : null}
-								/>
-							</li>
-						);
-					})}
-				</ul>
-			</section>
-		</Main>
-	);
+      <section className="mb-3 mt-3">
+        <h2 className="text-3xl font-bold text-center pt-3 pb-5 md:pt-0">
+          Mis Posts
+        </h2>
+        <ul className="flex flex-row flex-wrap basis-20 w-full justify-around gap-3 max-w-screen-2xl md:flex-nowrap">
+          {rents?.map((image) => {
+            const rentImages = images.find(
+              (item) => item.rentId === image.rent_id
+            );
+            return (
+              <li key={image.rent_id} className="w-full">
+                <Carousel
+                  images={rentImages?.length !== 0 ? rentImages : null}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </section>
+    </Main>
+  );
 }
